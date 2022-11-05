8a0b72bfb2eb91d82a76a0f5ea32b73bc62700e4
Roberto Camberos <42880184+rcamberos23@users.noreply.github.com>
2022-11-05 09:25:53 -0700

    add 739

---
+++ 739. Daily Temperatures.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0] * len(temperatures)
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][1]:
+                index, temp = stack.pop()
+                res[index] = i - index
+            
+            stack.append([i,t])
+        return res
