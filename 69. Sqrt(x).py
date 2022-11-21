22a33347d7e91f3398ada858548a3a80922a3d69
Roberto Camberos <42880184+rcamberos23@users.noreply.github.com>
2022-11-21 08:23:58 -0800

    add 69

---
+++ 69. Sqrt(x).py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        left = 2
+        right = x // 2
+        while left <= right:
+            mid = left + (right - left) // 2
+            num = mid * mid
+            if num < x:
+                left = mid + 1
+            elif num > x:
+                right = mid - 1
+            else:
+                return mid
+        return right
+                
